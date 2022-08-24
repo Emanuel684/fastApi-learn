741ceb9e0a98563e326bc61e2b617b9f8d6c282e
emanuel.acevedo <emanuel.acevedo@quipux.com>
2022-08-24 08:18:44 -0500

    New code to dockerfile

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from typing import Union
 from datetime import datetime
 import pytz
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,24 +8,12 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Hola": "Bienvenid@"}
 
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    print("read_item: ", item_id)
-    print("q read_item: ", q)
-    return {"item_id": item_id, "q": q}
-
-@app.get('/users/{user_id}')
-async def read_user(user_id: int):
-    return {"user_id": user_id}
-
-# docker run -d --name contfastapi -p 80:80 -v /etc/localtime:/etc/localtime:ro fastapi
-@app.get('/datetime2')
+@app.get('/datetime-host')
 async def set_datetime2():
     return {"datetime2": datetime.now()}
 
-@app.get('/datetime')
+@app.get('/datetime-pytz')
 async def set_datetime():
     return {"datetime": datetime.now(tz=pytz.timezone('America/Bogota'))}
